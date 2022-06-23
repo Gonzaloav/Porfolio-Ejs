ca4e11ef4606f5bd24d5d5f20cb47059b3dcf47c
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-23 13:39:45 +0200

    carrusel solucionado

--- static/js/carruselinicio.js
+++ static/js/carruselinicio.js
@@ -1,51 +1,50 @@
 
-function Carruselinico () {
-    const images = [ 
-        "/static/imagenes/inicio/paisaje.jpg", 
-        "/static/imagenes/inicio/fauna.jpg",
-        "/static/imagenes/inicio/retrato.jpg",
-        "/static/imagenes/inicio/conceptual.jpg"
-    ];
-    let selectedIndex = 0;
-    let selectedImage = images[0];
-  
-    const selectNewImage = (selectedIndex, images, next = true) => {
-     
-      const condition = next ? selectedIndex < images.length - 1 : selectedIndex > 0;
-  
-      const nextIndex = next ? condition  ? selectedIndex + 1 : 0 : condition ? selectedIndex - 1 : images.length - 1;
-  
-      setSelectedImage(images[nextIndex]);
-      selectedIndex = nextIndex;
-      console.log(
+
+const images = [ 
+    "/static/imagenes/inicio/paisaje.jpg", 
+    "/static/imagenes/inicio/fauna.jpg",
+    "/static/imagenes/inicio/retrato.jpg",
+    "/static/imagenes/inicio/conceptual.jpg"
+];
+
+let selectedIndex = 0;
+let selectedImage = images[0];
+
+const selectNewImage = (images, next = true) => {
+    
+    const condition = next ? selectedIndex < images.length - 1 : selectedIndex > 0;
+
+    const nextIndex = next ? condition  ? selectedIndex + 1 : 0 : condition ? selectedIndex - 1 : images.length - 1;
+
+    setSelectedImage(images[nextIndex]);
+    selectedIndex = nextIndex;
+    console.log(
         "nextIndex , selectedIndex ,selectedImage",
         selectedIndex,
         nextIndex,
         selectedImage,
-        setSelectedImage(images[nextIndex])
-      );
-    };
-  
-    const previous = () => {
-      selectNewImage(selectedIndex, images, false);
-    };
-  
-    const next = () => {
-      selectNewImage(selectedIndex, images);
-    };
-  
-    function setSelectedImage (newImage) {
-        const img = document.querySelector("#carrusel")
-        img.src = newImage
-    }
+    );
+};
 
-    function start () {
-        setInterval(() => {
-            selectNewImage(selectedIndex, images);
-        }, 3000);
-            return () => clearInterval(interval);
-    }
-        
-    document.addEventListener("load", start)
+const previous = () => {
+    console.log("Previous <-");
+    selectNewImage(images, false);
+};
+
+const next = () => {
+    console.log("Next ->");
+    selectNewImage(images);
+};
+
+function setSelectedImage (newImage) {
+    const img = document.querySelector("#carrusel")
+    img.src = newImage
 }
-    
+
+
+setInterval(() => {
+    selectNewImage(images);
+}, 3000);
+        
+document.querySelector("#nextButton").addEventListener("click",next)
+document.querySelector("#previousButton").addEventListener("click",previous)
